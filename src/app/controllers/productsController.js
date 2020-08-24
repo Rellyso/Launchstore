99e514c372e46d9db29e0aca080bc6f7598ac3a2
Rellyson <rellysondouglas2015@gmail.com>
2020-08-24 08:35:34 -0300

    added new functions

--- src/app/controllers/productsController.js
+++ src/app/controllers/productsController.js
@@ -1,5 +1,34 @@
+const Category = require('../models/Category')
+const Product = require('../models/Product')
+
 module.exports = {
     create(req, res) {
-        return res.render('products/create.njk')
-    }
+        Category.all()
+            .then(function (results) {
+
+                const categories = results.rows
+
+                return res.render('products/create.njk', { categories })
+            }).catch((err) => {
+                throw new Error(err)
+            })
+    },
+
+    async post(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (let key of keys) {
+            // req.body.avatar_url == ""
+            if (req.body[key] == "")
+                return res.send('Please fill in all fields.')
+        }
+
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+
+        results = await Category.all()
+        const categories = results.rows
+
+        res.render('products/create.njk', { productId, categories})
+    },
 }
