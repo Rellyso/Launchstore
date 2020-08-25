bc5f508aa9b43d261604b1b9e2b81ad431ab15e1
Rellyson <rellysondouglas2015@gmail.com>
2020-08-25 11:03:23 -0300

    Added put and delete methods

--- src/app/controllers/productsController.js
+++ src/app/controllers/productsController.js
@@ -1,5 +1,7 @@
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const { formatPrice } = require('../../lib/utils')
+const { put } = require('../../routes')
 
 module.exports = {
     create(req, res) {
@@ -22,14 +24,53 @@ module.exports = {
             if (req.body[key] == "")
                 return res.send('Please fill in all fields.')
         }
-        
+
         let results = await Product.create(req.body)
 
         const productId = results.rows[0].id
 
+        return res.redirect(`/products/${productId}/edit`)
+    },
+
+    async edit(req, res) {
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if (!product) return res.send('Product not found!!')
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
         results = await Category.all()
         const categories = results.rows
 
-        await res.render('products/create.njk', { productId, categories})
+        return res.render('products/edit.njk', { product, categories })
     },
+
+    async put(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (let key of keys) {
+            // req.body.avatar_url == ""
+            if (req.body[key] == "")
+                return res.send('Please fill in all fields.')
+        }
+
+        req.body.price = req.body.price.replace(/\D/g, "")
+
+        if (req.body.old_price != req.body.price) {
+            const oldProduct = await Product.find(req.body.id)
+
+            req.body.old_price = oldProduct.rows[0].price
+        }
+
+        await Product.update()
+
+        return res.redirect(`/products/${req.body.id}/edit`)
+    },
+
+    async delete(req, res) {
+        await Product.delete(req.body.id)
+
+        return res.redirect('/products')
+    }
 }
