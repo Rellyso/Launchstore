39dbb22d7e7ecb9e10654712491693b8dcbde4c2
Rellyson <rellysondouglas2015@gmail.com>
2020-09-25 10:29:24 -0300

    Added search's query function to receive filtered products

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -13,7 +13,7 @@ module.exports = {
             INSERT INTO products (
                 category_id,
                 user_id,
-                name,
+                name,]
                 description,
                 old_price,
                 price,
@@ -78,5 +78,35 @@ module.exports = {
 
     files(id) {
         return db.query(`SELECT * FROM files WHERE product_id = $1`, [id])
+    },
+
+    search(params) {
+        const { filter, category } = params
+
+        let query = "",
+            filterQuery = `WHERE`
+
+        if (category) {
+            filterQuery = `${filterQuery}
+                products.category_id = ${category}            
+                AND
+                `
+        }
+
+        filterQuery = `
+            ${filterQuery}
+            products.name ilike '%${filter}%'
+            OR products.description ilike '%${filter}%'
+        `
+
+        query = `
+            SELECT products.*, categories.name AS category_name
+            FROM products
+            LEFT JOIN categories ON (categories.id = products.category_id)
+            ${filterQuery}
+            GROUP BY products.id, categories.name
+        `
+
+        return db.query(query)
     }
 }
