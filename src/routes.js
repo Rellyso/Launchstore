1b6bebe545b989d3b1a11a16e38208b1a76430ea
Rellyson <rellysondouglas2015@gmail.com>
2020-09-25 10:22:07 -0300

    Getting new routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
-const ProductController = require('./app/controllers/productsController')
-const homeController = require('./app/controllers/homeController')
 
-routes.get('/', homeController.index)
+const ProductController = require('./app/controllers/ProductsController')
+const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
+
+routes.get('/', HomeController.index)
+
+//Controlador de pesquisa
+routes.get('/products/search', SearchController.index)
+
 
 //Controlador do produto
 routes.get('/products', ProductController.create)
@@ -14,6 +20,7 @@ routes.get('/products/:id/edit', ProductController.edit)
 routes.post('/products', multer.array("photos", 6), ProductController.post)
 routes.put('/products', multer.array("photos", 6), ProductController.put)
 routes.delete('/products', ProductController.delete)
+
 
 //Alias
 routes.get('/ads/create', function (req, res) {
