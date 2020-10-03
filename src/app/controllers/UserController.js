96f54682a9d284379063ec52c3cc00b4cf3331b0
Rellyson <rellysondouglas2015@gmail.com>
2020-10-03 09:45:39 -0300

    Getting async post  user to verify if exists an user with this email or cpf-cnpj

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,30 @@
+const User = require('../models/User')
+
 module.exports = {
     registerForm(req, res) {
         return res.render("users/register")
+    },
+    async post(req, res) {
+        const keys = Object.keys(req.body)
+
+        // verify 
+
+        // if was filled in all fields
+        for (let key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please fill in all fields.')
+            }
+        }
+
+        // if user exists [email, cpf_cnpj]
+        const { email, cpf_cnpj } = req.body
+
+        const user = await User.findOne({ 
+            where: { email },
+            cpf_cnpj: { cpf_cnpj }
+        })
+
+        return res.send(req.body)
+
     }
 }
