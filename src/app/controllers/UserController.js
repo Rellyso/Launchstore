9bbaadbd6a7ba03587b07119190c331d06d77e9a
Rellyson <rellysondouglas2015@gmail.com>
2020-10-05 20:20:48 -0300

    Getting full User controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,27 +4,14 @@ module.exports = {
     registerForm(req, res) {
         return res.render("users/register")
     },
+
+    show(req, res) {
+        return res.send('Cadastrado')
+    },
+
     async post(req, res) {
-        const keys = Object.keys(req.body)
+        const userId = await User.create(req.body)
 
-        // verify 
-
-        // if was filled in all fields
-        for (let key of keys) {
-            if (req.body[key] == "") {
-                return res.send('Please fill in all fields.')
-            }
-        }
-
-        // if user exists [email, cpf_cnpj]
-        const { email, cpf_cnpj } = req.body
-
-        const user = await User.findOne({ 
-            where: { email },
-            cpf_cnpj: { cpf_cnpj }
-        })
-
-        return res.send(req.body)
-
+        return res.redirect('/users')
     }
 }
