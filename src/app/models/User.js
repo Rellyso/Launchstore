12152282caaa82fd4f7cb5e974aa1ba6f0d24627
Rellyson <rellysondouglas2015@gmail.com>
2020-10-05 20:21:51 -0300

    Got hash encrypt for password | Added create query function

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db')
+const { hash } = require('bcryptjs')
 
 module.exports = {
     async findOne(filters) {
@@ -6,18 +7,54 @@ module.exports = {
             SELECT * FROM users
         `
 
-        Object.keys(fieters).map(key => {
+        Object.keys(filters).map(key => {
             query = `${query} 
             ${key}
-            ` 
+            `
 
             Object.keys(filters[key]).map(field => {
                 query = `${query} ${field} = '${filters[key][field]}'`
             })
         })
 
-        const results = await db.query(query)
+        try {
+            const results = await db.query(query)
 
-        return results.rows[0]
+            return results.rows[0] || null
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
+    async create(data) {
+        try {
+            const query = ` 
+            INSERT INTO users (
+                name,
+                email,
+                password,
+                cpf_cnpj,
+                cep,
+                address
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
+            // password hash
+            const passwordHash = await hash(data.password, 8)
+
+            const values = [
+                data.name,
+                data.email,
+                passwordHash,
+                data.cpf_cnpj.replace(/\D/g, ""),
+                data.cep.replace(/\D/g, ""),
+                data.address,
+            ]
+
+            const results = await db.query(query, values)
+            return results.rows[0].id
+        } catch (err) {
+            console.error(err)
+        }
     }
 }
