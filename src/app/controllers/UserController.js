e504f4fd0678ec861cb6667cd4a55bf31c2c79a6
Rellyson <rellysondouglas2015@gmail.com>
2020-10-06 17:20:10 -0300

    Updating user controller to receive session configs

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,8 @@ module.exports = {
     async post(req, res) {
         const userId = await User.create(req.body)
 
+        req.session.userId = userId
+
         return res.redirect('/users')
     }
 }
