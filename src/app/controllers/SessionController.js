198025965ea08b3a39b5f03f921c966e96d0daeb
Rellyson <rellysondouglas2015@gmail.com>
2020-10-13 13:59:02 -0300

    Adding forgot form and forgot post logic

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,6 @@
+const crypto = require('crypto')
+const User = require('../models/User')
+
 module.exports = {
     loginForm(req,res) {
         return res.render("session/login")
@@ -12,5 +15,24 @@ module.exports = {
         req.session.destroy()
 
         return res.redirect('/')
+    },
+    forgotForm(req,res) {
+        return res.render("session/forgot-password")
+    },
+    async forgot(req, res) {
+        // criar um token para o usuário 
+        const token = crypto.randomBytes(20).toString('hex')
+
+        // criar uma expiração
+        let now = new Date()
+        now = now.setHours(now.getHours() + 1)
+
+        await User.update(user.id, {
+            reset_token: token,
+            reset_token_expires: now,
+        })
+        // enviar um email com um link de recuperação de senha
+
+        // avisar ao usuário que enviamos um email
     },
 }
