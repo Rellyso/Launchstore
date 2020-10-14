f3bb63f431f606ab9e32cf32b239b2cf343750e0
Rellyson <rellysondouglas2015@gmail.com>
2020-10-14 18:51:40 -0300

    Getting routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -16,9 +16,9 @@ routes.post('/login', SessionValidator.login ,SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 // // reset password / forgot
-// routes.get('/password-reset', SessionController.resetForm)
+routes.get('/password-reset', SessionController.resetForm)
 routes.get('/forgot-password', SessionController.forgotForm)
-// routes.post('/password-reset', SessionController.reset)
+routes.post('/password-reset', SessionValidator.reset, SessionController.reset)
 routes.post('/forgot-password', SessionValidator.forgot, SessionController.forgot)
 
 
