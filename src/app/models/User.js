30eda8d41815ae417de041f64d78a0d515aa6856
Rellyson <rellysondouglas2015@gmail.com>
2020-11-30 11:20:45 -0300

    Getting Base query functions at User model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,70 +3,52 @@ const { hash } = require('bcryptjs')
 const fs = require('fs')
 
 const Product = require('./Product')
+const Base = require('./Base')
+
+Base.init({ table: 'users' })
 
 module.exports = {
-    async findOne(filters) {
-        let query = `
-            SELECT * FROM users
-        `
+    ...Base,
+    // async create(data) {
 
-        Object.keys(filters).map(key => {
-            query = `${query} 
-            ${key}
-            `
+    //     try {
+    //         const query = ` 
+    //         INSERT INTO users (
+    //             name,
+    //             email,
+    //             password,
+    //             cpf_cnpj,
+    //             cep,
+    //             address
+    //         ) VALUES ($1, $2, $3, $4, $5, $6)
+    //         RETURNING id
+    //     `
+    //         // password hash
+    //         const passwordHash = await hash(data.password, 8)
 
-            Object.keys(filters[key]).map(field => {
-                query = `${query} ${field} = '${filters[key][field]}'`
-            })
-        })
+    //         const values = [
+    //             data.name,
+    //             data.email,
+    //             passwordHash,
+    //             data.cpf_cnpj.replace(/\D/g, ""),
+    //             data.cep.replace(/\D/g, ""),
+    //             data.address,
+    //         ]
 
-        try {
-            const results = await db.query(query)
-
-            return results.rows[0] || null
-        } catch (err) {
-            console.log(err)
-        }
-    },
-
-    async create(data) {
-        try {
-            const query = ` 
-            INSERT INTO users (
-                name,
-                email,
-                password,
-                cpf_cnpj,
-                cep,
-                address
-            ) VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING id
-        `
-            // password hash
-            const passwordHash = await hash(data.password, 8)
-
-            const values = [
-                data.name,
-                data.email,
-                passwordHash,
-                data.cpf_cnpj.replace(/\D/g, ""),
-                data.cep.replace(/\D/g, ""),
-                data.address,
-            ]
-
-            const results = await db.query(query, values)
-            return results.rows[0].id
-        } catch (err) {
-            console.error(err)
-        }
-    },
+    //         const results = await db.query(query, values)
+    //         return results.rows[0].id
+    //     } catch (err) {
+    //         console.error(err)
+    //     }
+    // },
+    
     async update(id, fields) {
         let query = `UPDATE users SET`
 
         Object.keys(fields).map((key, index, array) => {
             if ((index + 1) < array.length) {
                 // if not last key
-                
+
                 query = ` ${query}
                     ${key} = '${fields[key]}',
                 `
@@ -83,20 +65,21 @@ module.exports = {
 
         return
     },
+    
     async delete(id) {
         try {
             // Pegar todos os produtos do usuário
             let results = await db.query("SELECT * FROM products WHERE user_id = $1", [id])
             const products = results.rows
-            
+
             // Pegar todas as imagens de cada produto
             const allFilesPromise = products.map(product => Product.files(product.id))
-            
+
             let promiseResults = await Promise.all(allFilesPromise)
-    
+
             // Deletar o usuário do bd
             await db.query('DELETE FROM users WHERE id = $1', [id])
-    
+
             // Excluir todos os arquivos da pasta public
             promiseResults.map(results => {
                 results.rows.map(file => {
