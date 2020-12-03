4ffaa490529aba14cf9626849a41730f200bd88e
Rellyson <rellysondouglas2015@gmail.com>
2020-12-03 12:08:25 -0300

    got createCategories and createProducts seeds

--- seed.js
+++ seed.js
@@ -2,14 +2,20 @@ const faker = require('faker')
 const { hash } = require('bcryptjs')
 
 const User = require('./src/app/models/User')
+const Product = require('./src/app/models/Product')
+const Category = require('./src/app/models/Category')
+const File = require('./src/app/models/File')
 
-let usersId = []
+let usersIds = []
+let totalCategories = 4
+let totalUsers = 3
+let totalProducts = 10
 
 async function createUsers() {
     const users = []
     const password = await hash('1111', 8)
 
-    while (users.length < 3) {
+    while (users.length < totalUsers) {
 
         users.push({
             name: faker.name.firstName(),
@@ -21,10 +27,65 @@ async function createUsers() {
         })
 
     }
-    
-    const usersPromise = users.map( user => User.create(user))
 
-    usersId = await Promise.all(usersPromise)
+    const usersPromise = users.map(user => User.create(user))
+
+    usersIds = await Promise.all(usersPromise)
+    console.log(usersIds)
 }
 
-createUsers()
+async function createCategories() {
+    let categories = []
+
+    while (categories.length < totalCategories) {
+        categories.push({
+            name: faker.name.title(),
+        })
+    }
+
+    const categoriesPromise = categories.map( category => Category.create(category)) 
+    await Promise.all(categoriesPromise)
+}
+
+async function createProducts() {
+    let products = []
+
+    while (products.length < totalProducts) {
+        products.push({
+            category_id: Math.ceil(Math.random() * totalCategories),
+            user_id: usersIds[Math.floor(Math.random() * totalUsers)],
+            name: faker.name.title(),
+            description: faker.lorem.paragraph(Math.ceil(Math.random() * 10)),
+            old_price: faker.random.number(999),
+            price: faker.random.number(999),
+            quantity: faker.random.number(99),
+            status: Math.round(Math.random())
+        })
+    }
+
+    const productsPromise = products.map(product => Product.create(product))
+    productsIds = await Promise.all(productsPromise)
+    console.log(productsIds)
+
+    let files = []
+
+    while (files.length < 50) {
+        files.push({
+            name: faker.image.image(),
+            path: 'public/images/placeholder.png',
+            product_id: productsIds[Math.floor(Math.random() * totalProducts)],
+        })
+    }
+
+    const filesPromise = files.map(file => File.create(file))
+
+    await Promise.all(filesPromise)
+}
+
+async function init() {
+    await createUsers()
+    await createCategories()
+    await createProducts()
+}
+
+init()
