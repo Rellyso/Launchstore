dc9e4701b041ff8f3fd2cf1f263e9ed1e38da07f
Rellyson <rellysondouglas2015@gmail.com>
2020-12-05 13:28:49 -0300

    updating controller to work well

--- src/app/controllers/homeController.js
+++ src/app/controllers/homeController.js
@@ -13,7 +13,7 @@ module.exports = {
 
         async function getImage(productId) {
             let files = await Product.files(productId)
-            files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+            files = files.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
 
             return files[0]
         }
