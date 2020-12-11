2f68c4c3397c55557facd24c979c68179651d0b5
Rellyson <rellysondouglas2015@gmail.com>
2020-12-11 11:29:19 -0300

    Updated search product query

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,3 +1,4 @@
+const { query } = require('../../config/db')
 const db = require('../../config/db')
 const Base = require('./Base')
 
@@ -5,40 +6,34 @@ Base.init({ table: 'products' })
 
 module.exports = {
     ...Base,
-        
+
     async files(id) {
         const results = await db.query(`SELECT * FROM files WHERE product_id = $1`, [id])
 
         return results.rows
     },
 
-    async search(params) {
-        const { filter, category } = params
+    async search({ filter, category }) {
 
-        let query = "",
-            filterQuery = `WHERE`
-
-        if (category) {
-            filterQuery = `${filterQuery}
-                products.category_id = ${category}            
-                AND
-                `
-        }
-
-        filterQuery = `
-            ${filterQuery}
-            products.name ilike '%${filter}%'
-            OR products.description ilike '%${filter}%'
-        `
-
-        query = `
-            SELECT products.*, categories.name AS category_name
+        let query = `
+            SELECT products.*,
+                categories.name AS category_name
             FROM products
             LEFT JOIN categories ON (categories.id = products.category_id)
-            ${filterQuery}
-            GROUP BY products.id, categories.name
+            WHERE 1 = 1
         `
 
+        if (category) {
+            query += ` AND products.category_id = ${category}`
+        }
+
+        if (filter) {
+            query += ` AND (products.name ILIKE '%${filter}%'
+            OR products.description ILIKE '%${filter}%')`
+        }
+
+        query += ` AND status != 0`
+        
         const results = await db.query(query)
 
         return results.rows
