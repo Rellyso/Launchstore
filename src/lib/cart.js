6d782ade372241cbe0c9beb408b0dd4ebf1a7c81
rellyso <rellysondouglas2015@gmail.com>
2021-01-25 09:06:02 -0300

    getting removeOne product function

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -50,7 +50,38 @@ const Cart = {
 
         return this
     },
-    removeOne(productId){},
+    removeOne(productId){
+        // pega o produto a ser removido a partir do id e coloca na variável inCart
+        const inCart = this.items.find(item => item.product.id == productId)
+
+        // se não existir o produto com o id a ser removido, retorna o carrinho sem alterações
+        if (!inCart) return this
+
+        // se existir o id, remover produto:
+        inCart.quantity--
+        inCart.price = inCart.product.price * inCart.quantity
+        inCart.formattedPrice = formatPrice(inCart.price)
+
+        // e remover do total do carrinho:
+        this.total.quantity--
+        this.total.price -= inCart.product.price
+        this.total.formattedPrice = formatPrice(this.total.price)
+
+        // se quantidade for igual a zero
+        if (inCart.quantity < 1) {
+            // const itemIndex = this.items.indexOf(inCart)
+            // this.items.splice(itemIndex, 1)
+            // return this
+
+            // traz todos os itens que não são de id's iguais 
+            this.items = this.items.filter(item => 
+                item.product.id != inCart.product.id)
+            return this
+        }
+
+        return this
+        
+    },
     delete(productId){},
 }
 
@@ -78,8 +109,12 @@ console.log('add third cart item')
 oldCart = Cart.init(oldCart).addOne(product2)
 console.log(oldCart)
 
-console.log('add last cart item')
-oldCart = Cart.init(oldCart).addOne(product)
+console.log('remove one cart item')
+oldCart = Cart.init(oldCart).removeOne(product.id)
+console.log(oldCart)
+
+console.log('remove one cart item 2')
+oldCart = Cart.init(oldCart).removeOne(product.id)
 console.log(oldCart)
 
 module.exports = Cart
