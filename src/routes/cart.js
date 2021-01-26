e8131cafbbe34070ef8fc43754e35397aa083c76
rellyso <rellysondouglas2015@gmail.com>
2021-01-26 11:32:51 -0300

    getting cart routes

---
+++ src/routes/cart.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const routes = express.Router()
+
+const CartController = require('../app/controllers/CartController')
+
+
+routes.get('/', CartController.index)
+    .post('/:id/add', CartController.addOne)
+
+module.exports = routes
