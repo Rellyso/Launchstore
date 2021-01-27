a5ee1c8000815c3d76d11aafac3186c24ad11e27
rellyso <rellysondouglas2015@gmail.com>
2021-01-27 15:14:50 -0300

    adding remove and delete routes

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -6,5 +6,7 @@ const CartController = require('../app/controllers/CartController')
 
 routes.get('/', CartController.index)
     .post('/:id/add', CartController.addOne)
+    .post('/:id/remove', CartController.removeOne)
+    .post('/:id/delete', CartController.delete)
 
 module.exports = routes
