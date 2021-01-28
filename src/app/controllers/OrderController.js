8fa8a7efaec77a7a0da8c1b3bd7713a89e635fb1
rellyso <rellysondouglas2015@gmail.com>
2021-01-28 10:58:54 -0300

    added index to show /orders page

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -4,6 +4,7 @@ const Order = require('../models/Order')
 
 const mailer = require('../../lib/mailer')
 const Cart = require('../../lib/cart')
+const { formatPrice, date } = require('../../lib/utils')
 
 const email = (seller, product, buyer) => `
 <h2>Olá, ${seller.name}</h2>
@@ -23,6 +24,51 @@ const email = (seller, product, buyer) => `
 `
 
 module.exports = {
+    async index(req, res) {
+        // pegar os pedidos do comprador
+        let orders = await Order.findAll({where: { id: req.session.userId }})
+
+        const getOrdersPromise = orders.map(async order => {
+            // detalhes do produto
+            order.product = await LoadProductsService.load('products', {
+                where: { id: order.product_id }
+            })
+
+            // detalhes do comprador
+            order.buyer = await User.findOne({
+                where: { id: order.buyer_id }
+            })
+
+            // detalhes do vendedor
+            order.seller = await User.findOne({
+                where: { id: order.seller_id }
+            })
+
+            // formatação de preços
+            order.formattedPrice = formatPrice(order.price)
+            order.formattedTotal = formatPrice(order.total)
+
+            // formatação de status
+            const statuses = {
+                open: "Aberto",
+                sold: "Vendido",
+                canceled: "Cancelado",
+            }
+
+            order.formattedStatus = statuses[order.status] // statuses.open
+
+            // formatação de atualizado em...
+            const updatedAt = date(order.updated_at)
+
+            order.formattedUpdatedAt = `${order.formattedStatus} em ${updatedAt.day}/${updatedAt.month}/${updatedAt.year} às ${updatedAt.hour}h${updatedAt.minutes}`
+
+            return order
+        })
+
+        orders = await Promise.all(getOrdersPromise)
+
+        return res.render("orders/index", { orders })
+    },
     async post(req, res) {
 
         try {
