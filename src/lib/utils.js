79684a050bc0aed60a86dbfcd9bd91b06de0543f
rellyso <rellysondouglas2015@gmail.com>
2021-01-28 10:59:07 -0300

    updating to get minutes and years

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,13 +7,14 @@ module.exports = {
         const month = `0${(date.getMonth() + 1)}`.slice(-2)
         const day = `0${date.getDate()}`.slice(-2)
         const hour = `0${date.getHours()}`.slice(-2)
-        const minute = `0${date.getMinutes()}`.slice(-2)
+        const minutes = `0${date.getMinutes()}`.slice(-2)
 
         return {
             day,
             month,
+            year,
             hour, 
-            minute,
+            minutes,
             iso: `${year}-${month}-${day}`,
             birthDay: `${day}/${month}`,
             format: `${day}/${month}/${year}`
